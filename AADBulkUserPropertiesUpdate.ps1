7e6eb1f05db3752a6f982ff46641957d1e82506b
Richard <88572955+RichardGithinji@users.noreply.github.com>
2022-04-17 22:33:04 +0300

    Updating the Manager
    #Updating the Manager
    #Updating the user infor

--- AADBulkUserPropertiesUpdate.ps1
+++ AADBulkUserPropertiesUpdate.ps1
@@ -1,27 +1,23 @@
 #connecting to the Azure AD
 Connect-AzureAD
 
-
-
 #importing the CSV source which has the changes
-$data = Import-Csv E:\Projects\Bulk-Update\Roster2.csv
-
-
+$data = Import-Csv E:\Projects\Bulk-Update\Roster3.csv
 
 #Iterating through each row in the CSV
 foreach ($row in $data)
 {
 #INFO in the Console
-Write-Host "Updating the user :" $row.'User Username' " manager to " $row.'Department' $row.'JobTitle' $row.'PhysicalDeliveryOfficeName' -ForegroundColor Yellow
-
-
-
+Write-Host "Updating the user :" $row.'User Username' "Manager" -ForegroundColor Yellow
 #Updating the Manager
+Set-AzureADUserManager -ObjectId (Get-AzureADUser -ObjectId $row.'User Username').Objectid -RefObjectId (Get-AzureADUser -ObjectId $row.'Manager Username').Objectid
+
+#INFO in the Console
+Write-Host "Updating " $row.'User Username'"s' infor" -ForegroundColor Yellow
 Set-AzureADUser -ObjectId (Get-AzureADUser -ObjectId $row.'User Username').Objectid -Department ($row.'Department') -JobTitle ($row.'JobTitle') -PhysicalDeliveryOfficeName ($row.'PhysicalDeliveryOfficeName')
 
-
-
 #Completion info in the console for the specified row
-Write-Host "Updated." -ForegroundColor Green
+Write-Host "updated." -ForegroundColor Green
 
 }
+Write-Host "...Update complete..." -ForegroundColor Green
